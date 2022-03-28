d864f2460b51cbc5cd4a69f106efe2e1416e3d6e
Elle Nolan <me@profl.io>
2022-03-28 18:41:17 -0400

    add updatePlayer mutation

--- src/mutation/index.ts
+++ src/mutation/index.ts
@@ -5,13 +5,17 @@ import createGame from "./createGame";
 import createMatch from "./createMatch";
 import createEvent from "./createEvent";
 
+import updatePlayer from "./updatePlayer";
+
 const mutation = {
-  createPlayer: createPlayer,
-  createOrganization: createOragnization,
-  createResult: createResult,
-  createGame: createGame,
-  createMatch: createMatch,
-  createEvent: createEvent,
+  createPlayer,
+  createOragnization,
+  createResult,
+  createGame,
+  createMatch,
+  createEvent,
+
+  updatePlayer,
 };
 
 export default mutation;
---
+++ src/mutation/updatePlayer.ts
@@ -0,0 +1,32 @@
+import { Playstyle, Pronoun } from "@prisma/client";
+import { GraphQLContext } from "../context";
+
+const updatePlayer = async (
+  parent: unknown,
+  args: {
+    playerName: string,
+    newName?: string,
+    newCountry?: string,
+    newPronouns?: Pronoun[],
+    newPlaystyles?: Playstyle[]
+  },
+  context: GraphQLContext
+) => {
+
+  return await context.prisma.player.update({
+    where: {
+      name: args.playerName,
+    },
+    data: {
+      name: args.newName,
+      country: args.newCountry,
+
+      // full overwrite of scalar
+      // TODO: additions w/o overwrite?
+      playstyles: args.newPlaystyles,
+      pronouns: args.newPronouns,
+    },
+  });
+};
+
+export default updatePlayer;
